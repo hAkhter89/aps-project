021da4e0a6d0e6e1eccb53306c59b94c4d9154e4
Hassan Akhter <akhterhassan89@gmail.com>
2024-11-24 05:13:24 +0500

    pieces

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 import pygame
+import auxilfuncs
 
 pygame.init()
 
@@ -37,7 +38,38 @@ selected = 100
 legal_moves = []
 
 #textures and assets
+bq = pygame.transform.scale(pygame.image.load('alpha/bQ.png'), (80,80))
+bq_small = pygame.transform.scale(bq, (45,45))
+bk = pygame.transform.scale(pygame.image.load('alpha/bK.png'), (80,80))
+bk_small = pygame.transform.scale(bk, (45,45))
+br = pygame.transform.scale(pygame.image.load('alpha/bR.png'), (80,80))
+br_small = pygame.transform.scale(br, (45,45))
+bb = pygame.transform.scale(pygame.image.load('alpha/bB.png'), (80,80))
+bb_small = pygame.transform.scale(bb, (45,45))
+bn = pygame.transform.scale(pygame.image.load('alpha/bN.png'), (80,80))
+bn_small = pygame.transform.scale(bn, (45,45))
+bp = pygame.transform.scale(pygame.image.load('alpha/bP.png'), (80,80))
+bp_small = pygame.transform.scale(bp, (45,45))
+wq = pygame.transform.scale(pygame.image.load('alpha/wQ.png'), (80,80))
+wq_small = pygame.transform.scale(wq, (45,45))
+wk = pygame.transform.scale(pygame.image.load('alpha/wK.png'), (80,80))
+wk_small = pygame.transform.scale(wk, (45,45))
+wr = pygame.transform.scale(pygame.image.load('alpha/wR.png'), (80,80))
+wr_small = pygame.transform.scale(wr, (45,45))
+wb = pygame.transform.scale(pygame.image.load('alpha/wB.png'), (80,80))
+wb_small = pygame.transform.scale(wb, (45,45))
+wn = pygame.transform.scale(pygame.image.load('alpha/wN.png'), (80,80))
+wn_small = pygame.transform.scale(wn, (45,45))
+wp = pygame.transform.scale(pygame.image.load('alpha/wP.png'), (80,80))
+wp_small = pygame.transform.scale(wp, (45,45))
 
+white_imgs = [auxilfuncs.wp, auxilfuncs.wq, auxilfuncs.wk, auxilfuncs.wn, auxilfuncs.wb, auxilfuncs.wr]
+white_imgs_small = [auxilfuncs.wp_small, auxilfuncs.wq_small, auxilfuncs.wk_small, auxilfuncs.wn_small, auxilfuncs.wb_small, auxilfuncs.wr_small]
+
+black_imgs = [auxilfuncs.bp, auxilfuncs.bq, auxilfuncs.bk, auxilfuncs.bn, auxilfuncs.bb, auxilfuncs.br]
+black_imgs_small = [auxilfuncs.bp_small, auxilfuncs.bq_small, auxilfuncs.bk_small, auxilfuncs.bn_small, auxilfuncs.bb_small, auxilfuncs.br_small]
+
+piece_list = ['pawn', 'queen', 'king', 'knight', 'bishop', 'rook']
 
 
                 
@@ -47,7 +79,7 @@ legal_moves = []
 #board color
 
 light_brown = (240,219,182)
-dark_brown = (55,26,8)
+dark_brown = (111,130,56)
 
 
 #drawing the main board
@@ -60,6 +92,24 @@ def draw_board():
             pygame.draw.rect(screen,light_brown,[600 - (column * 200), row* 100, 100, 100])
         else:
             pygame.draw.rect(screen,light_brown,[700 - (column * 200), row* 100, 100, 100])
+
+def draw_pieces():
+    for i in range(len(white_pieces)):
+        imgindex = piece_list.index(white_pieces[i])
+        if white_pieces[i] == 'pawn':
+            screen.blit(wp, (white_coords[i][0] * 100 + 8, white_coords[i][1] * 100 + 18))
+        else:
+            screen.blit(white_imgs[imgindex], (white_coords[i][0] * 100 + 8, white_coords[i][1] * 100 + 10))
+    
+    for i in range(len(black_pieces)):
+        imgindex = piece_list.index(black_pieces[i])
+        if black_pieces[i] == 'pawn':
+            screen.blit(bp, (black_coords[i][0] * 100 + 8, black_coords[i][1] * 100 + 18))
+        else:
+            screen.blit(black_imgs[imgindex], (black_coords[i][0] * 100 + 8, black_coords[i][1] * 100 + 10))
+            
+        
+        
 
 
 
@@ -83,6 +133,7 @@ while run == True:
     timer.tick(fps)
     screen.fill(dark_brown)
     draw_board()
+    draw_pieces()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
